a87d80350621331ee0d244f501023061e0951279
SteveGazzoJr <33587635+SteveGazzoJr@users.noreply.github.com>
2018-06-26 16:00:12 -0400

    Update remoteDiscloser.js
    added direct reference to script address in output

--- remoteDiscloser.js
+++ remoteDiscloser.js
@@ -1,2 +1,2 @@
-console.log("Remote script included and executed");
+console.log("Remote script from https://raw.githubusercontent.com/SteveGazzoJr/remoteJS/master/remoteDiscloser.js included and executed");
 console.log(sessionStorage.getItem("prStorage-session"));
